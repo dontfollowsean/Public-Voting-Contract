9a5c5ddcac01c74ef8c10e94cf62187cc6b8ba4d
Sean Wilkinson <swilkinson09@gmail.com>
2017-12-11 17:21:20 -0500

    Update contract deployment, add default gas value to truffle.js

--- truffle.js
+++ truffle.js
@@ -1,12 +1,13 @@
 // Allows us to use ES6 in our migrations and tests.
-require('babel-register')
+require('babel-register');
 
 module.exports = {
-  networks: {
-    development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*' // Match any network id
-    }
-  }
-}
+	networks: {
+		development: {
+			host: 'localhost',
+			port: 8545,
+			network_id: '*', // Match any network id
+			gas: 470000
+		}
+	}
+};
